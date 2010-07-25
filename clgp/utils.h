5ec661d48bbf2cc286f9c4bcd12185356403afee
Matthieu Volat <mazhe.github@alkumuna.eu>
2010-07-25 19:13:15 +0200

    Add utils function to allocate images and the pyramid

--- clgp/utils.h
+++ clgp/utils.h
@@ -16,6 +16,22 @@ clgpFirstDevice(cl_device_id *id);
 int
 clgpMaxflopsDevice(cl_device_id *id);
 
+/* Create a 2D image object, simplified version (for GPUs only) */
+cl_mem
+clgpCreateImage2D (
+        cl_mem_flags flags, 
+        const cl_image_format *image_format, 
+        size_t image_width, 
+        size_t image_height, 
+        cl_int *errcode_ret);
+
+/* Create a 2D image object that will store the pyramid (ie allocate
+ * an image of size width*3 x height) */
+cl_mem
+clgpCreatePyramid2D(
+        cl_mem image,
+        cl_int *errcode_ret);
+
 #ifdef __cplusplus
 }
 #endif
