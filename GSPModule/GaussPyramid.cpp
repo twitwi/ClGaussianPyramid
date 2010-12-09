e9121657ead450e6399af27eeeb934a17d12a331
Remi Emonet <remi.emonet@reverse--com.heeere>
2010-12-10 00:56:40 +0100

    operating gsp module (loose dependency to opencv)

--- GSPModule/GaussPyramid.cpp
+++ GSPModule/GaussPyramid.cpp
@@ -6,9 +6,9 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-#include <CL/cl.h>
 #include <clgp.h>
 #include <utils.h>
+#include <cv.h>
 
 /* Get the x origin of the level in the pyramid image */
 static int LEVEL_ORIGIN_X(int level, int width, int height) {
@@ -18,20 +18,84 @@ static int LEVEL_ORIGIN_X(int level, int width, int height) {
 static int LEVEL_ORIGIN_Y(int level, int width, int height) {
     return ((level <= 2) ? 0 : (level & 0x1)*(height>>(level>>1)));
 }
-
-void GaussPyramid::initModule() {
-}
-void GaussPyramid::stopModule() {
-}
-
 static double timeFrom(struct timeval start) {
     timeval stop;
     gettimeofday(&stop, NULL);
     double time = (stop.tv_sec-start.tv_sec)*1000.f + (stop.tv_usec-start.tv_usec)/1000.f;
     return time;
 }
+static double timeFromAndUpdate(struct timeval& start) {
+    timeval stop;
+    gettimeofday(&stop, NULL);
+    double time = (stop.tv_sec-start.tv_sec)*1000.f + (stop.tv_usec-start.tv_usec)/1000.f;
+    start = stop;
+    return time;
+}
+#define SAFE_FREE(a) {if (a) free(a);}
 
-void GaussPyramid::input(char* dataRGB, int w, int h) {
+
+GaussPyramid::GaussPyramid() {
+    context = 0;
+    queue = 0;
+    cache.w = -1;
+    cache.h = -1;
+    cache.inRGBA = NULL;
+    cache.outRGB = NULL;
+    cache.outRGBA = NULL;
+}
+
+void GaussPyramid::initModule() {
+    cl_int err = 0;
+    cl_device_id device;
+    struct timeval start;
+    gettimeofday(&start, NULL);
+
+    /* OpenCL init, using our utils functions */
+    clgpMaxflopsDevice(&device);
+
+    /* Create a context on this device */
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+    if (err != CL_SUCCESS) {
+        fprintf(stderr, "Could not create a context for the device\n");
+        exit(1);
+    }
+
+    /* Create a command queue for the library */
+    queue = clCreateCommandQueue(context, device, 0, &err);
+    if (err != CL_SUCCESS) {
+        fprintf(stderr, "Could not create a command queue for the device\n");
+        exit(1);
+    }
+
+    /* Initialize the clgp library */
+    if (clgpInit(context, queue) != 0) {
+        fprintf(stderr, "Could not init clgp library\n");
+        exit(1);
+    }
+    printf(" * Init time: %f ms\n", timeFrom(start));
+
+    SAFE_FREE(cache.inRGBA);
+    SAFE_FREE(cache.outRGB);
+    SAFE_FREE(cache.outRGBA);
+}
+void GaussPyramid::stopModule() {
+    struct timeval start;
+    gettimeofday(&start, NULL);
+    /* Release the clgp library */
+    clgpRelease();
+    /* Release device ressources */
+    clReleaseContext(context);
+    clReleaseCommandQueue(queue);
+    printf(" * Shutdown time: %f ms\n", timeFrom(start));
+}
+
+void GaussPyramid::input(IplImage *im) {
+    IplImage *rgba = cvCreateImage(cvSize(im->width, im->height), IPL_DEPTH_8U, 4);
+    cvCvtColor(im, rgba, CV_RGB2RGBA);
+    inputRGBA(rgba->imageData, im->width, im->height);
+    cvReleaseImage(&rgba);
+}
+void GaussPyramid::inputRGB(char* dataRGB, int w, int h) {
     char* dataRGBA = (char*) malloc(w*h*4);
     for(int y=0; y<h; y++)
         for(int x=0; x<w; x++) {
@@ -60,13 +124,8 @@ void GaussPyramid::outputBoth(char* dataRGBA, int wRef, int h) {
 }
 
 void GaussPyramid::inputRGBA(char* dataRGBA, int w, int h) {
+
     cl_int err = 0;
-
-    cl_device_id device;
-    cl_context context = 0;
-    cl_command_queue queue = 0;
-
-    //MagickWand *input_wand = NULL, *pyramid_wand;
     unsigned char *input_data = NULL, *pyramid_data;
     unsigned int input_width = 0, pyramid_width = 0;
     unsigned int input_height = 0, pyramid_height = 0;
@@ -79,79 +138,23 @@ void GaussPyramid::inputRGBA(char* dataRGBA, int w, int h) {
     size_t origin[3] = {0, 0, 0};
     size_t region[3] = {0, 0, 1};
 
-    struct timeval start, stop;
-    double compute_time = 0., total_time = 0.;
+    struct timeval start;
+    struct timeval step;
+    gettimeofday(&start, NULL);
+    step = start;
 
-
-    struct timeval full_start, full_stop;
-    double full_time;
-    gettimeofday(&full_start, NULL);
-
-
-    /* ImageMagick init */
-    //MagickWandGenesis();
-
-    /* OpenCL init, using our utils functions */
-    clgpMaxflopsDevice(&device);
-
-    /* Create a context on this device */
-    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "Could not create a context for the device\n");
-        exit(1);
-    }
-
-    /* Create a command queue for the library */
-    queue = clCreateCommandQueue(context, device, 0, &err);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "Could not create a command queue for the device\n");
-        exit(1);
-    }
-
-    /* Initialize the clgp library */
-    if (clgpInit(context, queue) != 0) {
-        fprintf(stderr, "Could not init clgp library\n");
-        exit(1);
-    }
-
-
-    /* Load image */
-    /*
-    input_wand = NewMagickWand();
-    if (MagickReadImage(input_wand, argv[1]) != MagickTrue) {
-        fprintf(stderr, "Could not load input image\n");
-        exit(1);
-    }
-    input_width = MagickGetImageWidth(input_wand);
-    input_height = MagickGetImageHeight(input_wand);
-    input_nbchannels = 4;
-    input_data = (unsigned char *)malloc(input_width*input_height*input_nbchannels*sizeof(char));
-    MagickExportImagePixels(
-            input_wand,
-            0,
-            0,
-            input_width,
-            input_height,
-            "RGBA",
-            CharPixel,
-            input_data);
-    */
     input_width = w;
     input_height = h;
     input_nbchannels = 4;
     input_data = (unsigned char*)dataRGBA;
 
-    /* Create pyramid image */
-    /*
-    pyramid_wand = NewMagickWand();
-    pyramid_width = 3 * input_width;
-    pyramid_height = input_height;
-    pyramid_nbchannels = input_nbchannels;
-    */
     pyramid_width = 3 * input_width;
     pyramid_height = input_height;
     pyramid_nbchannels = input_nbchannels;
     pyramid_data = (unsigned char *)malloc(pyramid_width*pyramid_height*input_nbchannels*sizeof(char));
+
+    printf("%d %d %d %lX\n", input_width, input_height, input_nbchannels, (long unsigned int) input_data);
+    printf(" * local allocation time: %f ms\n", timeFromAndUpdate(step));
 
     /* Create buffers on device */
     input_climage =
@@ -180,12 +183,10 @@ void GaussPyramid::inputRGBA(char* dataRGBA, int w, int h) {
             exit(1);
         }
     }
-    printf(" * Init time: %f ms\n", timeFrom(full_start));
 
-    fprintf(stderr, "%d %d %d %X\n", input_width, input_height, input_nbchannels, input_data);
+    printf(" * gpu allocation time: %f ms\n", timeFromAndUpdate(step));
 
     /* Send input image on device */
-    gettimeofday(&start, NULL);
     region[0] = input_width;
     region[1] = input_height;
     err = 
@@ -206,27 +207,19 @@ void GaussPyramid::inputRGBA(char* dataRGBA, int w, int h) {
         fprintf(stderr, "Could not copy input data on device (%d)\n", err);
         exit(1);
     }
-    gettimeofday(&stop, NULL);
-    total_time = 
-        (stop.tv_sec-start.tv_sec)*1000.f + (stop.tv_usec-start.tv_usec)/1000.f;
+    printf(" * sending image to gpu time: %f ms\n", timeFromAndUpdate(step));
 
 
     /* At last, call our pyramid function */
-    gettimeofday(&start, NULL);
     clgpBuildPyramid(
             pyramid_climage, 
             input_climage, 
             input_width, 
             input_height);
     clFinish(queue);
-    gettimeofday(&stop, NULL);
-    compute_time = 
-        (stop.tv_sec-start.tv_sec)*1000.f + (stop.tv_usec-start.tv_usec)/1000.f;
-    total_time += compute_time;
-
+    printf(" * running pyramid: %f ms\n", timeFromAndUpdate(step));
 
     /* Retrieve images */
-    gettimeofday(&start, NULL);
     for (level = 0; level < maxlevel; level++) {
         region[0] = input_width >> (level>>1);
         region[1] = input_height >> (level>>1);
@@ -250,52 +243,19 @@ void GaussPyramid::inputRGBA(char* dataRGBA, int w, int h) {
                 "Could not copy pyramid data on host (%d)\n", err);
         exit(1);
     }
-    gettimeofday(&stop, NULL);
-    total_time += 
-        (stop.tv_sec-start.tv_sec)*1000.f + (stop.tv_usec-start.tv_usec)/1000.f;
+    printf(" * retrieving pyramid: %f ms\n", timeFromAndUpdate(step));
+    printf(" * overall: %f ms\n", timeFrom(start));
+
+    /* Free images */
+    outputBoth((char*)pyramid_data, w, h);
+    //free(input_data);
+    free(pyramid_data);
 
 
-    /* Release the clgp library */
-    clgpRelease();
-
-
-    /* Release device ressources */
     clReleaseMemObject(input_climage);
     for (level = 0; level < maxlevel; level++) {
         clReleaseMemObject(pyramid_climage[level]);
     }
 
-    clReleaseContext(context);
-    clReleaseCommandQueue(queue);
-
-
-    gettimeofday(&full_stop, NULL);
-    full_time += 
-        (full_stop.tv_sec-full_start.tv_sec)*1000.f + (full_stop.tv_usec-full_start.tv_usec)/1000.f;
-
-    /* Show results */
-    printf(" * Pyramid time: %f ms\n", compute_time);
-    printf(" * Total time: %f ms\n", total_time);
-    printf(" * Full time: %f ms\n", full_time);
-    /* Display */
-    /*
-    MagickConstituteImage(
-            pyramid_wand, 
-            pyramid_width, 
-            pyramid_height, 
-            "RGBA", 
-            CharPixel, 
-            pyramid_data);
-    MagickSetImageOpacity(pyramid_wand, 1.0);
-    MagickDisplayImages(pyramid_wand, NULL);
-    */
-    /* Free images and ImageMagick */
-    outputBoth((char*)pyramid_data, w, h);
-    //free(input_data);
-    free(pyramid_data);
-    //DestroyMagickWand(input_wand);
-    //DestroyMagickWand(pyramid_wand);
-
-    //MagickWandTerminus();
 }
 
