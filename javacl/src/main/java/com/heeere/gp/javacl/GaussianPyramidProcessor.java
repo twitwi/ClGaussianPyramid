51eec511b417c644047c11dd2ac5e2f515eb0cff
Remi Emonet <remi.emonet@reverse--com.heeere>
2012-04-23 00:12:12 +0200

    bootstrap maven project using javacl

---
+++ javacl/src/main/java/com/heeere/gp/javacl/GaussianPyramidProcessor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.heeere.gp.javacl;
+
+/**
+ *
+ * @author twilight
+ */
+public class GaussianPyramidProcessor {
+    
+    
+    public static void main(String[] args) {
+    }
+}
