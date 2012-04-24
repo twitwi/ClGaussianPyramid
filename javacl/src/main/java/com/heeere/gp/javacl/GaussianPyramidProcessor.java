ccf50412ab3b4d921b2aad79ec14e90eedded984
Remi Emonet <remi.emonet@reverse--com.heeere>
2012-04-24 20:47:52 +0200

    added sqrt2 version (javacl) and more image examples

--- javacl/src/main/java/com/heeere/gp/javacl/GaussianPyramidProcessor.java
+++ javacl/src/main/java/com/heeere/gp/javacl/GaussianPyramidProcessor.java
@@ -25,9 +25,15 @@ public class GaussianPyramidProcessor {
     public static void main(String[] args) throws IOException {
         GaussianPyramidProcessor gpp = new GaussianPyramidProcessor();
 
+
         showTabbed(gpp.computePyramid(ImageIO.read(new File("../media/lena512color.png"))), true);
+        showTabbed(gpp.computeSqrt2Pyramid(ImageIO.read(new File("../media/lena512color.png"))), true);
         showTabbed(gpp.computePyramid(ImageIO.read(new File("../media/blurredbars.png"))), true);
         showTabbed(gpp.computePyramid(ImageIO.read(new File("../media/variablebars.png"))), true);
+        showTabbed(gpp.computePyramid(ImageIO.read(new File("../media/dots.png"))), true);
+        showTabbed(gpp.computeSqrt2Pyramid(ImageIO.read(new File("../media/dots.png"))), true);
+        showTabbed(gpp.computePyramid(ImageIO.read(new File("../media/dotpairs.png"))), true);
+        showTabbed(gpp.computeSqrt2Pyramid(ImageIO.read(new File("../media/dotpairs.png"))), true);
     }
     // Utilities
 
@@ -94,11 +100,51 @@ public class GaussianPyramidProcessor {
             last.waitFor();
         }
 
+        long afterGpu = System.currentTimeMillis();
         for (CLImage2D cLImage2D : L) {
             res.add(cLImage2D.read(queue));
             cLImage2D.release();
         }
-        System.err.println("Processed in " + (System.currentTimeMillis() - start) + "ms");
+        long now = System.currentTimeMillis();
+        System.err.println("Processed in " + (now - start) + "ms (incl. " + (afterGpu - start) + " without the read)");
+        return res;
+    }
+
+    public List<BufferedImage> computeSqrt2Pyramid(BufferedImage im) {
+        long start = System.currentTimeMillis();
+        int width = im.getWidth();
+        int height = im.getHeight();
+        CLImage2D input = context.createImage2D(CLMem.Usage.InputOutput, im, false);
+
+        int nLevels = 4 + 2 * Integer.numberOfTrailingZeros(Integer.highestOneBit(Math.max(width, height) / 5));
+        List<BufferedImage> res = new ArrayList<BufferedImage>();
+        List<CLImage2D> L = new ArrayList<CLImage2D>();
+        CLImageFormat imageFormat = new CLImageFormat(CLImageFormat.ChannelOrder.BGRA, CLImageFormat.ChannelDataType.UNormInt8);
+
+        L.add(input);
+        for (int l = 1; l < nLevels; l++) {
+            L.add(context.createImage2D(CLMem.Usage.InputOutput, imageFormat, levelWidthSqrt2(width, l), levelHeightSqrt2(height, l)));
+        }
+
+        { // doing it
+            CLEvent last = null;
+
+            last = queue9x9(L.get(0), L.get(1), last);
+            int i = 1;
+            while (i < nLevels - 2) {
+                last = queue5x5DownsampleCols(L.get(i), L.get(i + 1), last);
+                last = queue5x5DownsampleRows(L.get(i + 1), L.get(i + 2), last);
+                i += 2;
+            }
+            last.waitFor();
+        }
+        long afterGpu = System.currentTimeMillis();
+        for (CLImage2D cLImage2D : L) {
+            res.add(cLImage2D.read(queue));
+            cLImage2D.release();
+        }
+        long now = System.currentTimeMillis();
+        System.err.println("Processed in " + (now - start) + "ms (incl. " + (afterGpu - start) + " without the read)");
         return res;
     }
 
@@ -121,11 +167,41 @@ public class GaussianPyramidProcessor {
         return blockProcess;
     }
 
+    private CLEvent queue5x5DownsampleCols(CLImage2D input, CLImage2D output, CLEvent... previousToWaitFor) {
+        int width = (int) input.getWidth();
+        int height = (int) input.getHeight();
+        int[] localWorkSize = new int[]{(width >= 32) ? 16 : width >> 1, (height >= 32) ? 16 : height >> 1};
+        int[] globalWorkSize = new int[]{(((width >> 1) - 1) / localWorkSize[0] + 1) * localWorkSize[0], ((height - 1) / localWorkSize[1] + 1) * localWorkSize[1]};
+        CLEvent blockProcess = downsampledgauss5x5.downsampledgauss5x5_cols(queue, output, input, globalWorkSize, localWorkSize, previousToWaitFor);
+        return blockProcess;
+    }
+
+    private CLEvent queue5x5DownsampleRows(CLImage2D input, CLImage2D output, CLEvent... previousToWaitFor) {
+        int width = (int) input.getWidth();
+        int height = (int) input.getHeight();
+        int[] localWorkSize = new int[]{(width >= 32) ? 16 : width >> 1, (height >= 32) ? 16 : height >> 1};
+        int[] globalWorkSize = new int[]{((width - 1) / localWorkSize[0] + 1) * localWorkSize[0], (((height >> 1) - 1) / localWorkSize[1] + 1) * localWorkSize[1]};
+        CLEvent blockProcess = downsampledgauss5x5.downsampledgauss5x5_rows(queue, output, input, globalWorkSize, localWorkSize, previousToWaitFor);
+        return blockProcess;
+    }
+
     private int levelWidth(int base, int level) {
         return base >> ((level - 1) >> 1);
     }
 
     private int levelHeight(int base, int level) {
+        return base >> ((level - 1) >> 1);
+    }
+
+    private int levelWidthSqrt2(int base, int level) {
+        int res = base >> ((level - 1) >> 1);
+        if (level % 2 == 0) {
+            res >>= 1;
+        }
+        return res;
+    }
+
+    private int levelHeightSqrt2(int base, int level) {
         return base >> ((level - 1) >> 1);
     }
 }
